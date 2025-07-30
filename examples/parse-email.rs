cd0b608e1b427ad0f251d6ef2945f5d30304979d
baztar <taras_bazyuk@hotmail.com>
2025-07-30 22:03:30 +0300

    update example

--- examples/parse-email.rs
+++ examples/parse-email.rs
@@ -1,8 +1,8 @@
-use msg_parser::Outlook;
+use msg_parser::OldOutlook;
 
 fn main() {
     // Create Outlook object
-    let outlook = Outlook::from_path("data/test_email_4.msg").unwrap();
+    let outlook = OldOutlook::from_path("data/test_email_4.msg").unwrap();
 
     // Flush as json string
     let _json_string = outlook.to_json();
